4048da94403c11a60e4f42db40adbcd73089d867
Carlos <carlosiniesta.apps@gmail.com>
2020-01-16 14:33:55 +0100

    User model updated

--- app/User.php
+++ app/User.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    //
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'password', 'photo'];
+
+    public function create($request){
+        $user = new User;
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->photo = $request->photo;
+        $user->save();
+    }
 }
