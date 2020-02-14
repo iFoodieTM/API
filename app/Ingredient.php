16b0b4923d310fb413d3666eab98c1f3b53c490e
Andres_pinon <andres_constantino_pinon_apps1ma1819@cev.com>
2020-02-14 14:08:27 +0100

    created ingredient controller  fixed error  on usercontroller

--- app/Ingredient.php
+++ app/Ingredient.php
@@ -8,4 +8,30 @@ class Ingredient extends Model
 {
     protected $table = 'ingredients';
     protected $fillable = ['id','name'];
+
+    public function create_ingredient($request){
+
+        $ingredient = new Ingredient;
+
+        $ingredient->name = $request->name;
+  
+        $ingredient->save();
+        
+    }
+
+    public function ingredient_exist($name){
+
+        
+        $ingredients = self::where('name',$name)->get();
+        
+        foreach ($ingredients as $key => $value) {
+            if($value->name == $name){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+
 }
