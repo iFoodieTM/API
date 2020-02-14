64937ec1a2ce79b71b302d1b257b98fc70b94363
javierFab <javier_fabregas_apps1ma1819@cev.com>
2020-02-14 12:12:35 +0100

    Created Category controller, updated Category model

--- app/Category.php
+++ app/Category.php
@@ -9,4 +9,12 @@ class Category extends Model
     protected $table = 'categories';
     protected $fillable = ['id','name'];
 
+
+	public function create($request){
+
+		$category = new Category
+		$category->name = $request->name;
+		$category->photo = $request->photo;
+		$category->save();
+		
 }
